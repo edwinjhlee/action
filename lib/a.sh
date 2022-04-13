2f3bfcc71a698ee08ea6f5bd4307ee5737a3e775
B!gRice <hejiajie@x-cmd.com>
2022-04-13 15:43:28 +0800

    test jo

--- lib/a.sh
+++ lib/a.sh
@@ -1,2 +1,2 @@
 type x
-x jo [1, 2 3 {a: a, a1:123,abv:[1 2 3] ,d:{d1:12, d2:asc,d3:true , d4:{e1:true,2e:null}}}  ]
+x jo '[1, 2 3 {a: a, a1:123,abv:[1 2 3] ,d:{d1:12, d2:asc,d3:true , d4:{e1:true,2e:null}}}  ]'
