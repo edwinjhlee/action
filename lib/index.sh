f283a031229e7fba776a7d8a63a53fd80be758cb
L <l@x-cmd.com>
2022-04-21 13:50:33 +0800

    ...

--- lib/index.sh
+++ lib/index.sh
@@ -5,15 +5,6 @@ init_x_cmd(){
     eval "$(curl https://get.x-cmd.com/dev 2>/dev/null)" 2>/dev/null || true
 }
 
-init_ssh_key(){
-    [ -z "$ssh_key" ] && return
-    echo "github.com,52.74.223.119 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg=" >> ~/.ssh/known_hosts
-    echo "gitee.com,180.97.125.228 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBMuEoYdx6to5oxR60IWj8uoe1aI0X1fKOHWOtLqTg1tsLT1iFwXV5JmFjU46EzeMBV/6EmI1uaRI6HiEPtPtJHE=" >> ~/.ssh/known_hosts
-    echo "$ssh_key" >> ~/.ssh/id_rsa
-    chmod 600 ~/.ssh/known_hosts ~/.ssh/id_rsa
-    ssh-add ~/.ssh/id_rsa
-}
-
 init_git(){
     [ -n "$git_user" ] && git config --global user.name "$git_user"
     [ -n "$git_email" ] && git config --global user.email "$git_email"
@@ -32,12 +23,21 @@ init_docker(){
     true
 }
 
+init_ssh_key(){
+    [ -z "$ssh_key" ] && return
+    echo "github.com,52.74.223.119 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg=" >> ~/.ssh/known_hosts
+    echo "gitee.com,180.97.125.228 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBMuEoYdx6to5oxR60IWj8uoe1aI0X1fKOHWOtLqTg1tsLT1iFwXV5JmFjU46EzeMBV/6EmI1uaRI6HiEPtPtJHE=" >> ~/.ssh/known_hosts
+    echo "$ssh_key" >> ~/.ssh/id_rsa
+    chmod 600 ~/.ssh/known_hosts ~/.ssh/id_rsa
+    ssh-add ~/.ssh/id_rsa
+}
+
 init_main(){
     ___X_CMD_IN_CHINA_NET=
     init_x_cmd
+    init_docker
     init_ssh_key
     init_git
-    init_docker
     true
 }
 
