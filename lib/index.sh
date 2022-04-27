6114ad26da81e541f6664181fa89b54839dfe9b1
Zhengqbbb <1074059947@qq.com>
2022-04-27 16:16:42 +0800

    feat(github_token): add github_token input

--- lib/index.sh
+++ lib/index.sh
@@ -5,22 +5,23 @@ set +o errexit
 # Section: init
 ___x_cmd_ghaction_init_x_cmd(){
     # x log :init "x-cmd/dev"
-    eval "$(curl https://raw.githubusercontent.com/x-bash/get/main/index.html 2>/dev/null)" 2>/dev/null || true
+    eval "$(curl https://raw.githubusercontent.com/x-bash/get/dev/index.html 2>/dev/null)" 2>/dev/null || true
     # eval "$(curl https://get.x-cmd.com 2>/dev/null)" 2>/dev/null || true
 }
 
 ___x_cmd_ghaction_init_git_clone_current(){
     if [ -n "$ws_repo" ] && [ -n "$ws_ref" ]; then
-        # git clone https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}
-
-        local owner="${ws_repo%/*}"
         local repo="${ws_repo#*/}"
-
-        local url="https://${owner}:${GITHUB_TOKEN}@github.com/${repo}"
+        local url
+        if [ -n "$github_token" ]; then
+            local owner="${ws_repo%/*}"
+            url="https://${owner}:${github_token}@github.com/${ws_repo}.git"
+        else
+            url="https://github.com/${ws_repo}.git"
+        fi
 
         x log :init "git: cloning [ref=$ws_ref] from [url=$url]"
         git clone --branch "$ws_ref" "$url" && {
-            git_url="${git_url##*/}"
             x log :init "git: Creating [link=$(pwd)/ws] to [target=$(pwd)/$repo]"
             ln -s "$(pwd)/$repo" "$(pwd)/ws"
         }
@@ -59,14 +60,14 @@ ___x_cmd_ghaction_init_ssh_key(){
     eval "$(ssh-agent)"
     mkdir -p ~/.ssh
 
-    [ -z "$ssh_key" ] && return
-
     printf "%s\n" "
 github.com,52.74.223.119 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg=
 github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl
 github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==
 gitee.com,180.97.125.228 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBMuEoYdx6to5oxR60IWj8uoe1aI0X1fKOHWOtLqTg1tsLT1iFwXV5JmFjU46EzeMBV/6EmI1uaRI6HiEPtPtJHE=
 " >> ~/.ssh/known_hosts
+
+    [ -z "$ssh_key" ] && return
 
     printf "%s\n" "$ssh_key" >> ~/.ssh/id_rsa
     chmod 600 ~/.ssh/known_hosts ~/.ssh/id_rsa
